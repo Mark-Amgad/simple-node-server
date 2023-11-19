fdc9e80bf9cb8d3512b836186d4b52151deef3f7
Mark-Amgad-Mossad <mark.amgad@sylndr.com>
2023-11-19 21:20:11 +0200

    feat(build)

---
+++ build/index.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Mark is here");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const port = process.env.PORT || 3000;
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(body_parser_1.default.json());
+app.get("/", (req, res) => {
+    res.send({ message: "Hello from nodejs server" });
+});
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
